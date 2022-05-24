910c313a03580cc938b6dea613f1da66dc30da7a
ben-hulme <106058724+ben-hulme@users.noreply.github.com>
2022-05-24 08:52:59 +0800

    Update day4-rock-paper-scissors.py

--- day4-rock-paper-scissors.py
+++ day4-rock-paper-scissors.py
@@ -31,15 +31,16 @@ import random
 # List to link the choice with the ACSII Art rps = Rock, Paper, Scissors.
 rps_list = [rock, paper, scissors]
 print_result = ""
-
-# Winning Results Calculation Lists
-#     R    P    S
-R = ["D", "L", "W"]
-P = ["W", "D", "L"]
-S = ["L", "W", "D"]
-
-# Nested List of the above table for the player_choice
-nested_list = [R, P, S]
+# Winning Results Calculation Lists (Player = Rows, Computer = Columns)
+#    R    P    S
+#R=["D", "L", "W"]
+#P=["W", "D", "L"]
+#S=["L", "W", "D"]
+results_list = [
+  ["Draw", "You Loose", "You Win"],
+  ["You Win", "Draw", "You Loose"],
+  ["You Loose", "You Win", "Draw"],
+]
 
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
@@ -47,18 +48,19 @@ player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2
 if player_choice > 2 or player_choice < 0:
   print("Invalid Choice.")
 else:
+
   # Computer random choice
   computer_choice = random.randint(0, 2)
-  
-  # Going through the nested_list then into the "Winning Results Calculation Lists"
-  result = nested_list[player_choice][computer_choice]
+
+  #Navigating through the table to find the result.
+  result = results_list[player_choice][computer_choice]
   
   # If Statements to display the result of the game
-  if result == "D":
+  if result == "Draw":
     print_result = "Draw"
-  elif result == "L":
+  elif result == "You Loose":
     print_result = "You Loose"
-  elif result == "W":
+  elif result == "You Win":
     print_result = "You Win"
   
   # Printing of the Result of the Game
